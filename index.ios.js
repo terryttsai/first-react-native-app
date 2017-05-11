b8d7a321aad13cb48912cb0fb56c62a831ecf7bf
Tery Tsai <iliveinabucket@berkeley.edu>
2017-05-10 17:30:55 -0700

    Added react-native-mapbox-gl to iOS

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class TerryReactNative extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native Terry!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
